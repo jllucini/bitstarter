8497ecede1e50b6b4216ba2a059a6ff1594150a6
Jose Luis Lucini <jllucini@gmail.com>
2013-07-07 10:41:21 +0000

    Fixing web.js

--- web.js
+++ web.js
@@ -1,7 +1,8 @@
-var bIndex  = fs.readFileSync('index.html');
+var fs 	    = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var bIndex = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
   response.send(bIndex.toString());
