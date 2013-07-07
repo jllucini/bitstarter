30e9a5bd6907653580016ecb54e519e32e9860fb
Jose Luis Lucini <jllucini@gmail.com>
2013-07-07 10:21:17 +0000

    Changed web.js to read form index.html

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
+var bIndex  = fs.readFileSync("index.html");
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(bIndex.toString());
 });
 
 var port = process.env.PORT || 5000;
